48b7432c4cc570020d603786a5be26ef3e66af2e
james <james@mcmurran.org>
2015-06-07 10:30:15 -0400

    First commit

---
+++ API/Alerts.php
@@ -0,0 +1,43 @@
+<?php
+ini_set('display_errors',1);  error_reporting(E_ALL);
+
+/*
+      "Country": "Archenland",
+      "City": "",
+      "region": "",
+      "province":"",
+      "Issued":"1433609674",
+      "level":"Travel through airport and by bus for work only",
+      "current":"Yes",
+      "reason":"Recent Flooding",
+      "reasonLong":"River Winding Arrow, a large bordering river that runs West to East, recently flooded portions of Archenland. Flash floods and dangerous conditions are currently still being reported",
+      "lat":"-40.44452",
+      "lng":"-64.17245",
+      
+      
+      "Date": "1433609674"*/
+
+
+
+include '.././include/db.php';
+
+$query = "SELECT * FROM `Alerts`";
+
+//execute the query. 
+
+$result = $mysqli->query($query); 
+
+//display information: 
+
+$stingOut = '';
+while($row = mysqli_fetch_array($result)) { 
+  $stingOut .= json_encode ($row) . ',';
+}
+echo '{
+  "Locations": [
+    '.rtrim($stingOut, ",").'
+  ],
+  "Date": "'.time().'"
+}';
+
+?> 
---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+
+<html>
+        
+    </script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimal-ui">
+    
+   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+    <script src="lib/jquery.min.js"></script>
+    <script src="lib/jquery.mobile.js"></script>
+    
+    <style>
+        .titleImage {
+          margin: 4%;
+          max-height: 200px;
+        }
+        .ui-listview>li p {
+           white-space: normal;
+        }
+        .hidden{
+            display:none;
+        }
+    </style>
+    
+    <script>
+    
+    var Last;
+    $( window ).load(function() {
+        $( "ul" ).on( "click", "li", function() {
+          $( ".hidden", this ).slideToggle( "slow" );
+        });
+    });
+    
+    
+    function timeConverter(UNIX_timestamp){
+      var a = new Date(UNIX_timestamp*1000);
+      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes();
+      var sec = a.getSeconds();
+      var time = month + ',' + date + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+      return time;
+    }
+    
+    
+    $.get( "API/Alerts.php", function( data ) {
+        console.log(data);
+        $('ul').empty();
+        $('#date').text(timeConverter(data.Date));
+        $.each( data.Locations, function( key, val ) {
+            console.log( "<li id='" + key + "'>" + val + "</li>" );
+            
+            $( "#locationList" ).append( '<li>\
+                <span class="country">'+val.Country+'</span>\
+                <span class="city">, '+val.City+'</span>\
+                <span class="region">, '+val.region+'</span>\
+                <br />\
+                <span class="hidden">\
+                  Province :'+val.province+'<br />\
+                  Issued :'+val.Issued+'<br />\
+                  Level :'+val.level+'<br />\
+                  Current :'+val.current+'<br />\
+                  Reason :'+val.reason+'<br />\
+                  <p>Full Reason: '+val.reasonLong+'<p><br/>\
+                  lat:'+val.lat+'<br />\
+                  lng:'+val.lng+'<br />\
+                </span>\
+            </li>' );
+        });
+      $('ul' ).listview( "refresh" );
+    }, "json" );
+    
+    
+    
+    </script>
+  </head>
+
+<body><span>Login</span>
+<span class="ng-scope" style="text-align: center"><img class="titleImage" src="img/peacecorps_logo-home.png" /></span>
+     
+<form class="ui-filterable">
+    <input id="filterBasic-input" data-type="search">
+</form>
+<ul id="locationList" data-role="listview" data-filter="true" data-input="#filterBasic-input">
+
+</ul>
+
+</body>
+</html>
